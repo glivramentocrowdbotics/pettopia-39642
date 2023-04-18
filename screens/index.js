055cbb980547f08e5cbfdd7639d73cd2302be18e
Crowdbotics <team@crowdbotics.com>
2023-04-18 01:40:02 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,102 +1,23 @@
-import React, { createRef, useState } from "react";
-import { View, TouchableOpacity, Text, PermissionsAndroid, Platform, Alert } from "react-native";
 // @ts-ignore
-import CameraRoll from "@react-native-community/cameraroll";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-// @ts-ignore
-import { FilterCameraView } from "react-native-filter-camera";
-import * as script from "./utils";
-import { styles } from "./options";
-import Filter from "./components/Filter";
-import Loader from "./components/Loader";
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import { Provider } from "react-redux";
+import App from "./App";
+import rootReducer from "./Store";
 
-export const App = () => {
-  const [switchFlash, setSwitchFlash] = useState(false);
-  const [switchCamera, setSwitchCamera] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const cameraRef = createRef();
+const store = configureStore({
+  reducer: rootReducer
+});
 
-  const selectFilter = (settings) => {
-    script.setFilter(cameraRef, [settings]);
-  };
-
-  const switchCameraDirection = () => {
-    script.switchCameraDirection(cameraRef);
-    setSwitchCamera(!switchCamera);
-  };
-
-  const switchFlashLight = () => {
-    script.switchFlash(cameraRef);
-    setSwitchFlash(!switchFlash);
-  };
-
-  const handleTakePicture = () => {
-    setIsLoading(true);
-    const fileName = "id_" + new Date().getTime();
-    script.takePicture(cameraRef, [fileName]);
-  };
-
-  const hasAndroidPermission = async () => {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-    const hasPermission = await PermissionsAndroid.check(permission);
-    if (hasPermission) {
-      return true;
-    }
-    const status = await PermissionsAndroid.request(permission);
-    return status === "granted";
-  };
-
-  const onPictureTaken = async (pic) => {
-    const picUri = pic?.nativeEvent?.uri;
-    if (Platform.OS === "android" && !(await hasAndroidPermission())) {
-      return;
-    }
-    await CameraRoll.save(picUri, { type: "photo" });
-    setIsLoading(false);
-    Alert.alert(
-      "Info",
-      "Photo saved in Gallery.",
-      [
-        {
-          text: "Ok"
-        }
-      ],
-      { cancelable: false }
-    );
-  };
-
+const PhotoEditing = () => {
   return (
-    <View style={styles.cameraContainer}>
-      <FilterCameraView ref={cameraRef} onPictureTaken={onPictureTaken} style={styles.cameraContainer} />
-      <View style={styles.filterContainer}>
-        {isLoading && <Loader />}
-        <View style={styles.cameraUIView}>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={switchCameraDirection}
-          >
-            <Text style={{ color: "white" }}>{switchCamera ? <Icon name="camera-rear-variant" size={25} color="#fff" /> : <Icon name="camera-front-variant" size={25} color="#fff" />}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={handleTakePicture}
-          >
-            <Text style={{ color: "white" }}><Icon name="camera-iris" size={50} color="#fff" /></Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.menuButton}
-            onPress={switchFlashLight}
-          >
-            <Text style={{ color: "white" }}>{switchFlash ? <Icon name="flash" size={25} color="#fff" /> : <Icon name="flash-off" size={25} color="#fff" />}</Text>
-          </TouchableOpacity>
-        </View>
-        <Filter selectFilter={selectFilter} />
-      </View>
-    </View>
+    <Provider store={store}>
+      <App />
+    </Provider>
   );
 };
 
 export default {
-  title: "Live Camera Filters",
-  navigator: App
+  title: "Photo editing",
+  navigator: PhotoEditing
 };
--- screens/options.js
+++ screens/options.js
@@ -1,66 +1,238 @@
-import { StyleSheet } from "react-native";
-export const styles = StyleSheet.create({
-  cameraContainer: {
-    width: "100%",
-    height: "100%"
-  },
-  cameraUIView: {
-    marginBottom: 20,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    width: "100%"
-  },
-  menuButton: {},
-  filterContainer: {
-    width: "100%",
-    marginTop: -190,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent"
-  }
-});
 
-export const FILTERS = [
+const ratio = [
+  {
+    label: "1.1",
+    horizontal_ratio: 1,
+    vertical_ratio: 1,
+    dimensions: {
+      width: 30.26,
+      height: 32
+    }
+  }, {
+    label: "2.3",
+    horizontal_ratio: 2,
+    vertical_ratio: 3,
+    dimensions: {
+      width: 17.97,
+      height: 32
+    }
+  }, {
+    label: "3.2",
+    horizontal_ratio: 3,
+    vertical_ratio: 2,
+    dimensions: {
+      width: 30.26,
+      height: 19
+    }
+  }, {
+    label: "3.4",
+    horizontal_ratio: 3,
+    vertical_ratio: 4,
+    dimensions: {
+      width: 21.75,
+      height: 32
+    }
+  }, {
+    label: "4.3",
+    horizontal_ratio: 4,
+    vertical_ratio: 3,
+    dimensions: {
+      width: 30.26,
+      height: 24
+    }
+  }, {
+    label: "4.5",
+    horizontal_ratio: 4,
+    vertical_ratio: 5,
+    dimensions: {
+      width: 25.53,
+      height: 32
+    }
+  }
+];
+
+const settings = [
+  {
+    name: "contrast",
+    value: 1,
+    minValue: -10.0,
+    maxValue: 10.0
+  },
+  {
+    name: "saturation",
+    value: 1,
+    minValue: 0.0,
+    maxValue: 2
+  },
+  {
+    name: "brightness",
+    value: 1,
+    minValue: 0,
+    maxValue: 5
+  },
+  {
+    name: "temperature",
+    value: 6500,
+    minValue: 0.0,
+    maxValue: 40000.0
+  }
+];
+
+const FILTERS = [
   {
     id: 1,
-    settings: 2,
-    name: "Negative"
+    name: "Warm",
+    settings: {
+      blur: 0,
+      brightness: 1,
+      contrast: 1,
+      hue: 0,
+      negative: 0,
+      saturation: 1.609375,
+      sepia: 0,
+      sharpen: 0,
+      temperature: 6500
+    }
   },
   {
     id: 2,
-    settings: 4,
-    name: "Sharp"
+    name: "Classic",
+    settings: {
+      blur: 0,
+      brightness: 1,
+      contrast: 1.40625,
+      hue: 0,
+      negative: 0,
+      saturation: 1,
+      sepia: 0.78125,
+      sharpen: 0,
+      temperature: 6500
+    }
   },
   {
     id: 3,
-    settings: 6,
-    name: "Vintage"
+    name: "Vintage",
+    settings: {
+      blur: 0,
+      brightness: 1,
+      contrast: 0.46875,
+      hue: 1.8703125,
+      negative: -0.3125,
+      saturation: 0.09375,
+      sepia: 0.9375,
+      sharpen: 0,
+      temperature: 0
+    }
   },
   {
     id: 4,
-    settings: 8,
-    name: "Flare"
+    name: "Sharp",
+    settings: {
+      blur: 0,
+      brightness: 1,
+      contrast: 1,
+      hue: 0,
+      negative: 0,
+      saturation: 1,
+      sepia: 0,
+      sharpen: 1.5234375,
+      temperature: 6500
+    }
   },
   {
     id: 5,
-    settings: 10,
-    name: "Iris"
+    name: "Negative",
+    settings: {
+      blur: 0,
+      brightness: 1,
+      contrast: 1,
+      hue: 0,
+      negative: 0.90625,
+      saturation: 1,
+      sepia: 0,
+      sharpen: 0,
+      temperature: 6500
+    }
   },
   {
     id: 6,
-    settings: 12,
-    name: "Amaro"
+    name: "Bright",
+    settings: {
+      blur: 0,
+      brightness: 2.03125,
+      contrast: 1,
+      hue: 0,
+      negative: 0,
+      saturation: 1,
+      sepia: 0,
+      sharpen: 0,
+      temperature: 6500
+    }
   },
   {
     id: 7,
-    settings: 14,
-    name: "Lomo"
+    name: "Cool",
+    settings: {
+      blur: 0,
+      brightness: 0.98125,
+      contrast: 0.46875,
+      hue: 6.3,
+      negative: -0.5,
+      saturation: 0.234375,
+      sepia: -1.71875,
+      sharpen: 0,
+      temperature: 5625
+    }
+  }
 
+];
+
+const SHADOWS = [
+  {
+    id: 1,
+    url: "https://i.imgur.com/SzbbUvX.png"
   },
   {
-    id: 8,
-    settings: 16,
-    name: "Sepia"
+    id: 2,
+    url: "https://i.imgur.com/0PkQEk1.png"
+  },
+  {
+    id: 3,
+    url: "https://i.imgur.com/z2CQHpg.png"
+  },
+  {
+    id: 4,
+    url: "https://i.imgur.com/k9Eview.png"
+  },
+  {
+    id: 5,
+    url: "https://i.imgur.com/wh0On3P.png"
   }
+
 ];
+
+const blurShadows = [
+  {
+    id: 1,
+    name: "Blur",
+    value: 3,
+    minValue: 0,
+    maxValue: 10.0
+  },
+  {
+    id: 2,
+    name: "Blur Passes",
+    value: 6,
+    minValue: 1,
+    maxValue: 15.0
+  }
+
+];
+
+export default {
+  ratio: ratio,
+  settings: settings,
+  FILTERS: FILTERS,
+  SHADOWS: SHADOWS,
+  blurShadows: blurShadows
+};
