464162d7ae94b09aa7dc4b699fc951d90adc2b43
Crowdbotics <team@crowdbotics.com>
2023-04-18 01:37:13 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -3,60 +3,111 @@ import {
   Text,
   StyleSheet,
   View,
+  SafeAreaView,
   TextInput,
-  Image,
+  Switch,
   ScrollView
 } from "react-native";
 
-const InviteFriendsScreen = params => {
-  const [value, setValue] = useState("");
+const AccountSettingsScreen = (params) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [notifications, setNotifications] = useState(false);
+  const [emailNotifications, setEmailNotifications] = useState(false);
+  const [smsNotifications, setSmsNotifications] = useState(false);
+  const [deactivateAccount, setDeactivateAccount] = useState(false);
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.searchBar}>
-          <Text style={styles.searchText}>Search</Text>
-          <View
-            style={{
-              borderWidth: 1,
-              borderRadius: 10,
-              borderColor: "#C4C4C4",
-              flexDirection: "row",
-              alignItems: "center"
-            }}>
-            <View
-              style={{
-                width: "90%"
-              }}>
-              <TextInput
-                style={textStyles.input}
-                placeholder="Enter"
-                value={value}
-                onChangeText={text => setValue(text)}
-                placeholderTextColor="#ddd"
-              />
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.subContainer}>
+          <Text style={styles.subText}>Subscription</Text>
+          <View style={styles.subPallet}>
+            <View style={styles.planDes}>
+              <Text style={[styles.fnt25, styles.boldText]}>Plan</Text>
+              <Text style={styles.fnt16}>Description</Text>
             </View>
-            <Image source={require("./assets/search.png")} />
+            <View style={styles.subPricing}>
+              <Text style={[styles.fnt25, styles.boldText]}>$14.99 </Text>
+              <Text style={styles.fnt16}>per month</Text>
+            </View>
           </View>
         </View>
-        <View></View>
-        <View style={styles.frequently}>
-          <Text style={styles.frequentlyText}>Send Invite</Text>
+        <View style={styles.billingContainer}>
+          <Text style={styles.billingText}>Billing information</Text>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Full Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your full name"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+          </View>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Email Address</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Email Address"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View style={styles.nameInput}>
+            <Text style={styles.inputText}>Card number</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your Card Number"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+              value={cardNumber}
+              onChangeText={(text) => setCardNumber(text)}
+            />
+          </View>
         </View>
-        <View>
-          <Follower name="Cody Fisher" bgcolor="#D9DADD" invite={true} />
-          <Follower name="Johnny watson" bgcolor="#FCF1D6" invite={true} />
-          <Follower name="Jenny Wilson" bgcolor="#F9D8D9" invite={true} />
+        <View style={styles.togglesContainer}>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={notifications}
+              onValueChange={(value) => setNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Email Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={emailNotifications}
+              onValueChange={(value) => setEmailNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>SMS Notifications</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={smsNotifications}
+              onValueChange={(value) => setSmsNotifications(value)}
+            />
+          </View>
+          <View style={styles.toggle}>
+            <Text style={styles.toggleText}>Deactivate Account</Text>
+            <Switch
+              style={styles.toggleSwitch}
+              value={deactivateAccount}
+              onValueChange={(value) => setDeactivateAccount(value)}
+            />
+          </View>
         </View>
-        <View style={styles.frequently}>
-          <Text style={styles.frequentlyText}>More</Text>
-        </View>
-        <View>
-          <Follower name="Anthony" bgcolor="#D9DADD" />
-          <Follower name="Andres" bgcolor="#F9D8D9" />
-          <Follower name="Ander" bgcolor="#FCF1D6" />
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -65,87 +116,94 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  searchBar: {
-    padding: 20
-  },
-  searchText: {
-    marginLeft: 10,
-    marginBottom: 10
-  },
-  text: {
-    marginLeft: 30,
-    marginBottom: 10
-  },
-  frequently: {
-    height: 50,
-    width: "100%",
-    backgroundColor: "#DADADA",
-    flexDirection: "column",
+  subContainer: {
+    paddingHorizontal: 20,
+    flex: 0.2,
     justifyContent: "center"
   },
-  frequentlyText: {
-    marginLeft: 30,
-    color: "#8F8D86"
-  }
-});
-export default InviteFriendsScreen;
-
-const Follower = props => {
-  return (
-    <View style={FollowerStyles.follower}>
-      <View style={FollowerStyles.main}>
-        <View
-          style={[
-            FollowerStyles.image,
-            {
-              backgroundColor: props.bgcolor
-            }
-          ]}>
-          <Image source={require("./assets/edit.png")} />
-        </View>
-        <Text>{props.name}</Text>
-      </View>
-      {props.invite && <Text>Invite</Text>}
-    </View>
-  );
-};
-
-const FollowerStyles = StyleSheet.create({
-  follower: {
-    marginHorizontal: 20,
-    borderBottomWidth: 0.5,
-    borderBottomColor: "rgba(0,0,0,0.5)",
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 10,
-    justifyContent: "space-between"
+  subText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
   },
-  main: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  image: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    marginRight: 15,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
-const textStyles = StyleSheet.create({
-  input: {
+  subPallet: {
     backgroundColor: "#fff",
-    height: 53,
-    color: "#000",
     borderRadius: 10,
-    fontSize: 14,
-    paddingHorizontal: 10
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
-  error: {
-    fontSize: 13,
-    color: "#FA060D",
-    paddingTop: 8
+  planDes: {
+    flex: 0.4,
+    padding: 10,
+    // borderWidth: 1,
+    // borderColor: '#979797',
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  subPricing: {
+    flex: 0.6,
+    padding: 10,
+    // borderWidth: 1,
+    // borderColor: '#979797',
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center"
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  fnt25: {
+    fontSize: 25
+  },
+  fnt16: {
+    fontSize: 16
+  },
+  billingContainer: {
+    flex: 0.5,
+    paddingHorizontal: 20 // borderWidth: 1,
+    // borderColor: '#979797',
+  },
+  billingText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
+  },
+  inputText: {
+    fontSize: 16,
+    marginLeft: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    borderRadius: 10,
+    padding: 10,
+    paddingLeft: 20,
+    marginVertical: 10,
+    width: "100%"
+  },
+  togglesContainer: {
+    flex: 0.3,
+    paddingHorizontal: 20
+  },
+  toggle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  toggleText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 2,
+    marginVertical: 12,
+    marginLeft: 20
   }
 });
+export default AccountSettingsScreen;
