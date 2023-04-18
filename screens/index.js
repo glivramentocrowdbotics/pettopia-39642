cf81b68d35752a2fb2b8ea88a0d3191d73a826e9
Crowdbotics <team@crowdbotics.com>
2023-04-18 01:37:39 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,155 +1,238 @@
-import React, { useState, useEffect } from "react";
-import { Text, StyleSheet, View, Image, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Pressable
+} from "react-native";
 
-const NotificationsScreen = (params) => {
-  const [notifications, setNotifications] = useState([]);
-  useEffect(() => {
-    setNotifications([
-      {
-        id: 1,
-        type: "Booking Successful",
-        details: "You have booked Kreamy Corner",
-        time: "5 min ago",
-        read: false
-      },
-      {
-        id: 2,
-        type: "Booking Successful",
-        details: "You have booked Kreamy Corner",
-        time: "5 min ago",
-        read: false
-      },
-      {
-        id: 3,
-        type: "Event Reminder",
-        details: "Your next event will be held after 2 hours.",
-        time: "5 min ago",
-        read: true
-      },
-      {
-        id: 4,
-        type: "Event Reminder",
-        details: "Your next event will be held after 2 hours.",
-        time: "5 min ago",
-        read: true
-      },
-      {
-        id: 5,
-        type: "Event Reminder",
-        details: "Your next event will be held after 2 hours.",
-        time: "5 min ago",
-        read: true
-      },
-      {
-        id: 6,
-        type: "Event Reminder",
-        details: "Your next event will be held after 2 hours.",
-        time: "5 min ago",
-        read: true
-      },
-      {
-        id: 7,
-        type: "Event Reminder",
-        details: "Your next event will be held after 2 hours.",
-        time: "5 min ago",
-        read: true
-      }
-    ]);
-  }, []);
+const AddPaymentMethodScreen = (params) => {
+  const [paymentOption, setPaymentOption] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardExpiry, setCardExpiry] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [name, setName] = useState("");
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.headerTextContainer}>
-          <Text style={styles.headerText}>Select all</Text>
-          <Text style={styles.headerText}>Mark all</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <View style={styles.paletteContainer}>
+            <View style={styles.unSelected}></View>
+            <View style={styles.unSelected}></View>
+            <View style={styles.selected}></View>
+          </View>
+          <Image
+            source={require("./assets/3Dots.png")}
+            style={styles.threeDots}
+          />
         </View>
-      </View>
-      <View style={styles.notificationsContainer}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {notifications.map((notification, index) => (
-            <NotificationTile notification={notification} key={index} />
-          ))}
-        </ScrollView>
-      </View>
-    </View>
+        <View style={styles.inputs}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Payment Options</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setPaymentOption(text)}
+              value={paymentOption}
+              placeholder="Master Card"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Card Number</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setCardNumber(text)}
+              value={cardNumber}
+              placeholder="Enter your Card Number"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.halfInputs}>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputText}>Expiration Date</Text>
+              <TextInput
+                style={[styles.input, styles.input1]}
+                onChangeText={(text) => setCardExpiry(text)}
+                value={cardExpiry}
+                placeholder="Enter your last name"
+                placeholderTextColor="#9B9B9B"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <Text style={styles.inputText}>CVV</Text>
+              <TextInput
+                style={[styles.input, styles.input2]}
+                onChangeText={(text) => setCvv(text)}
+                value={cvv}
+                placeholder="CVV"
+                placeholderTextColor="#9B9B9B"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+            </View>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Card Holder Name</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setName(text)}
+              value={name}
+              placeholder="Username"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.checkBoxContainer}>
+            <Text style={styles.inputText}>Save this card details</Text>
+            <Image
+              source={require("./assets/checkbox.png")}
+              style={styles.checkBox}
+            />
+          </View>
+        </View>
+        <View style={styles.btnContainer}>
+          <Pressable style={styles.btn}>
+            <Text style={styles.btnText}>Continue</Text>
+            <Image
+              source={require("./assets/arrow.png")}
+              style={styles.arrow}
+            />
+          </Pressable>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
-const NotificationTile = ({ notification }) => {
-  const textColor = {
-    color: notification.read ? "#8E8E8E" : "#000"
-  };
-  return (
-    <View style={notificationTileStyles.container}>
-      <View style={notificationTileStyles.notificationTextContainer}>
-        <Text style={[notificationTileStyles.mainText, textColor]}>
-          {notification.type}
-        </Text>
-        <Text style={textColor}>{notification.details}</Text>
-      </View>
-      <View style={notificationTileStyles.timeContainer}>
-        <Text style={textColor}>{notification.time}</Text>
-        <View>
-          {!notification.read
-            ? (
-            <Image source={require("./assets/readIcon.png")} />
-              )
-            : null}
-        </View>
-      </View>
-    </View>
-  );
-};
-
-const notificationTileStyles = StyleSheet.create({
-  container: {
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    height: 100,
-    alignItems: "center"
-  },
-  notificationTextContainer: {
-    flexDirection: "column",
-    height: "80%",
-    justifyContent: "space-around" // alignItems: "center"
-  },
-  mainText: {
-    fontSize: 18,
-    marginVertical: 10
-  },
-  timeContainer: {
-    height: "80%",
-    justifyContent: "space-around",
-    alignItems: "center"
-  }
-});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
   },
   header: {
-    flex: 1,
-    paddingVertical: 10,
-    justifyContent: "space-around",
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc"
+    padding: 20
   },
-  headerTextContainer: {
+  paletteContainer: {
+    flexDirection: "row",
+    backgroundColor: "#F1F1F1",
+    height: 45,
+    width: "100%",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 5
+  },
+  selected: {
+    backgroundColor: "#fff",
+    height: "80%",
+    flex: 1,
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    marginHorizontal: 5
+  },
+  unSelected: {
+    height: "80%",
+    flex: 1,
+    marginHorizontal: 5,
+    backgroundColor: "#12D790",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10
+  },
+  threeDots: {
+    alignSelf: "center",
+    marginTop: 20
+  },
+  inputs: {
+    paddingHorizontal: 20,
+    justifyContent: "center"
+  },
+  inputContainer: {
+    flexDirection: "column",
+    flex: 1,
+    justifyContent: "center"
+  },
+  inputText: {
+    fontSize: 14,
+    marginLeft: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    borderRadius: 10,
+    padding: 10,
+    paddingLeft: 20,
+    marginVertical: 10,
+    width: "100%"
+  },
+  halfInputs: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    flex: 1
+  },
+  input1: {
+    borderRightWidth: 0,
+    borderRightColor: "#fff",
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    flex: 0.8
+  },
+  input2: {
+    borderLeftWidth: 0,
+    borderLeftColor: "#fff",
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    flex: 0.2
+  },
+  checkBoxContainer: {
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    padding: 10,
+    marginVertical: 10,
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: 20
+    borderRadius: 10
   },
-  headerText: {
-    fontSize: 18,
+  btnContainer: {
+    padding: 30,
+    paddingTop: 10,
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    marginTop: 20
+  },
+  btn: {
+    backgroundColor: "black",
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  arrow: {
+    marginLeft: 10,
+    marginTop: 2
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
     fontWeight: "bold"
-  },
-  notificationsContainer: {
-    flex: 12,
-    backgroundColor: "#fff",
-    paddingHorizontal: 20
   }
 });
-export default NotificationsScreen;
+export default AddPaymentMethodScreen;
