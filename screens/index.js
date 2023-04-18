ff77ebabd2f2cd89528544f3065ee6f81c94d119
Crowdbotics <team@crowdbotics.com>
2023-04-18 01:37:12 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,120 +1,146 @@
-import React from "react";
-import { Text, View, StyleSheet, TouchableHighlight, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  ScrollView
+} from "react-native";
 
-const SupportSendFeedbackScreen = () => {
+const InviteFriendsScreen = params => {
+  const [value, setValue] = useState("");
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.labelText}>
-          Email address
-        </Text>
-        <Input
-          placeholder='Enter'
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.searchBar}>
+          <Text style={styles.searchText}>Search</Text>
+          <View
+            style={{
+              borderWidth: 1,
+              borderRadius: 10,
+              borderColor: "#C4C4C4",
+              flexDirection: "row",
+              alignItems: "center"
+            }}>
+            <View
+              style={{
+                width: "90%"
+              }}>
+              <TextInput
+                style={textStyles.input}
+                placeholder="Enter"
+                value={value}
+                onChangeText={text => setValue(text)}
+                placeholderTextColor="#ddd"
+              />
+            </View>
+            <Image source={require("./assets/search.png")} />
+          </View>
+        </View>
+        <View></View>
+        <View style={styles.frequently}>
+          <Text style={styles.frequentlyText}>Send Invite</Text>
+        </View>
+        <View>
+          <Follower name="Cody Fisher" bgcolor="#D9DADD" invite={true} />
+          <Follower name="Johnny watson" bgcolor="#FCF1D6" invite={true} />
+          <Follower name="Jenny Wilson" bgcolor="#F9D8D9" invite={true} />
+        </View>
+        <View style={styles.frequently}>
+          <Text style={styles.frequentlyText}>More</Text>
+        </View>
+        <View>
+          <Follower name="Anthony" bgcolor="#D9DADD" />
+          <Follower name="Andres" bgcolor="#F9D8D9" />
+          <Follower name="Ander" bgcolor="#FCF1D6" />
+        </View>
       </View>
-      <View style={styles.Textarea}>
-        <Text style={styles.labelText}>
-          Message
-        </Text>
-        <TextInput style={styles.TextAreaInput}
-          numberOfLines={5}
-          multiline={true}
-          placeholder='Enter'
-          placeholderTextColor='#ddd'
-        />
-      </View>
-
-      <View style={styles.SubmitBtn}>
-        <Button>Submit</Button>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
     flex: 1,
-    height: "100%",
-    backgroundColor: "white"
+    backgroundColor: "#fff"
   },
-  labelText: {
-    fontSize: 14,
-    fontWeight: "400",
-    marginBottom: 10,
-    marginLeft: 15
+  searchBar: {
+    padding: 20
   },
-  Textarea: {
-    marginTop: 30
-  },
-  TextAreaInput: {
-    height: 160,
-    borderWidth: 1,
-    borderColor: "#C4C4C4",
-    borderRadius: 10,
-    textAlignVertical: "top",
-    padding: 10
-  },
-  SubmitBtn: {
-    marginTop: 100
-  }
-});
-
-export default SupportSendFeedbackScreen;
-
-const Button = (props) => {
-  return (
-    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
-      <View style={[btnStyles.button, {
-        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
-        height: props.height ? props.height : 49,
-        borderWidth: props.borderWidth ? props.borderWidth : 0,
-        borderColor: props.borderColor ? props.borderColor : "#000000"
-      }]}>
-        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
-      </View>
-    </TouchableHighlight>
-  );
-};
-
-const btnStyles = StyleSheet.create({
-  button: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10
+  searchText: {
+    marginLeft: 10,
+    marginBottom: 10
   },
   text: {
-    fontWeight: "bold",
-    fontSize: 15
+    marginLeft: 30,
+    marginBottom: 10
+  },
+  frequently: {
+    height: 50,
+    width: "100%",
+    backgroundColor: "#DADADA",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  frequentlyText: {
+    marginLeft: 30,
+    color: "#8F8D86"
   }
 });
+export default InviteFriendsScreen;
 
-const Input = (props) => {
+const Follower = props => {
   return (
-    <View>
-      <TextInput
-        style={textStyles.input}
-        placeholder={props.placeholder}
-        value={props.value}
-        onChangeText={(num) => props.setValue(num)}
-        placeholderTextColor='#ddd'
-        editable={props.editable !== false}
-      />
-      {props.errorText ? <Text style={textStyles.error}>{props.errorText}</Text> : null}
+    <View style={FollowerStyles.follower}>
+      <View style={FollowerStyles.main}>
+        <View
+          style={[
+            FollowerStyles.image,
+            {
+              backgroundColor: props.bgcolor
+            }
+          ]}>
+          <Image source={require("./assets/edit.png")} />
+        </View>
+        <Text>{props.name}</Text>
+      </View>
+      {props.invite && <Text>Invite</Text>}
     </View>
   );
 };
 
+const FollowerStyles = StyleSheet.create({
+  follower: {
+    marginHorizontal: 20,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "rgba(0,0,0,0.5)",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    justifyContent: "space-between"
+  },
+  main: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    marginRight: 15,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 const textStyles = StyleSheet.create({
   input: {
     backgroundColor: "#fff",
     height: 53,
-    borderColor: "#C4C4C4",
     color: "#000",
     borderRadius: 10,
     fontSize: 14,
-    borderWidth: 1,
     paddingHorizontal: 10
   },
   error: {
