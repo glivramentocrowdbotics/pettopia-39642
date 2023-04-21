340ad6465ab7a2e8c5c605efb3077dbb65047940
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-21 11:44:10 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/navigation-tabs/options.js
+++ modules/navigation-tabs/options.js
@@ -1,42 +1,47 @@
+import { StyleSheet } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { View, Text } from "react-native";
 import React from "react";
 
 function Feed() {
-  return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  return <View style={_styles.ZFWFlZCI}>
             <Text>Feed!</Text>
-        </View>
-  );
+        </View>;
 }
 
-const tabList = [
-  {
-    name: "Feed",
-    component: Feed,
-    label: "Feed",
-    tabIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="view-gallery" color={color} size={size} />
-    )
-  },
-  {
-    name: "Profile",
-    component: Feed,
-    label: "Profile",
-    tabIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-    )
-  },
-  {
-    name: "Notifications",
-    component: Feed,
-    label: "Updates",
-    tabIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account-child-outline" color={color} size={size} />
-    )
-  }
-];
-
+const tabList = [{
+  name: "Feed",
+  component: Feed,
+  label: "Feed",
+  tabIcon: ({
+    color,
+    size
+  }) => <MaterialCommunityIcons name="view-gallery" color={color} size={size} />
+}, {
+  name: "Profile",
+  component: Feed,
+  label: "Profile",
+  tabIcon: ({
+    color,
+    size
+  }) => <MaterialCommunityIcons name="account" color={color} size={size} />
+}, {
+  name: "Notifications",
+  component: Feed,
+  label: "Updates",
+  tabIcon: ({
+    color,
+    size
+  }) => <MaterialCommunityIcons name="account-child-outline" color={color} size={size} />
+}];
 export default {
   tabList: tabList
 };
+
+const _styles = StyleSheet.create({
+  ZFWFlZCI: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
